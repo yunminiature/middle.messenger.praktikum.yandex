378cfac274c673c33ba440a9e94a0cbcaeb5f31f
Yulia <yuliazhmakina@gmail.com>
2025-06-04 16:02:30 +0300

    fix: vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    root: '.',
-    build: {
-        outDir: 'dist',
-    },
-    server: {
-        port: 3000,
-        open: true,
-    },
-    preview: {
-        port: 3000
-    }
-})
+  root: '.',
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+  preview: {
+    port: 3000,
+  },
+});
