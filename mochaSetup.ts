4c059583a227820b2266caf116890ad82c5bcf35
Yulia <yuliazhmakina@gmail.com>
2025-07-18 13:42:53 +0300

    fix: gh test fixes

--- mochaSetup.ts
+++ mochaSetup.ts
@@ -1,37 +1,37 @@
 import { JSDOM } from 'jsdom';
 
 const dom = new JSDOM('<!doctype html><html><body></body></html>', {
-    url: 'http://localhost/',
+  url: 'http://localhost/',
 });
 
 const window = dom.window as unknown as Window & typeof globalThis;
 
 Object.defineProperty(global, 'window', {
-    value: window,
-    writable: true,
+  value: window,
+  writable: true,
 });
 
 Object.defineProperty(global, 'document', {
-    value: window.document,
-    writable: true,
+  value: window.document,
+  writable: true,
 });
 
 Object.defineProperty(global, 'HTMLElement', {
-    value: window.HTMLElement,
-    writable: true,
+  value: window.HTMLElement,
+  writable: true,
 });
 
 Object.defineProperty(global, 'Node', {
-    value: window.Node,
-    writable: true,
+  value: window.Node,
+  writable: true,
 });
 
 Object.defineProperty(global, 'MouseEvent', {
-    value: window.MouseEvent,
-    writable: true,
+  value: window.MouseEvent,
+  writable: true,
 });
 
 Object.defineProperty(global, 'getComputedStyle', {
-    value: window.getComputedStyle,
-    writable: true,
+  value: window.getComputedStyle,
+  writable: true,
 });
